a568ca0fc8e656813ca61dcb5c5e86d9d08f0976
harutiro <harutiro2027@outlook.jp>
2024-06-13 09:35:41 +0900

    動くコードができた。

--- src/応用編/ex1/ex2/ex2.ino
+++ src/応用編/ex1/ex2/ex2.ino
@@ -1,4 +1,4 @@
-カソードコモン
+//カソードコモン
 // chap5.1.2
 int ANODEPIN[] = {2, 3, 4, 5, 6, 7, 8, 9};
 int CATHODEPIN[] = {10, 11, 12, 14, 15, 16, 17, 18};
@@ -27,13 +27,12 @@ int mat_B[8][8] = {
 
 void setup(){
     delay(1000); // 初期化処理待ち
-    for (int ano = 0; ano < 8; ano++)
-    {
+    for (int ano = 0; ano < 8; ano++){
         pinMode(ANODEPIN[ano], OUTPUT);
         digitalWrite(ANODEPIN[ano], LOW);
     }
-    for (int cat = 0; cat < 8; cat++)
-    {
+    
+    for (int cat = 0; cat < 8; cat++){
         pinMode(CATHODEPIN[cat], OUTPUT);
         digitalWrite(CATHODEPIN[cat], HIGH);
     }
@@ -41,12 +40,9 @@ void setup(){
 
 void loop(){
     unsigned long ms = millis();
-    if ((ms / 1000) % 2 == 1)
-    {
+    if ((ms / 1000) % 2 == 1){
         showMatrix(mat_A);
-    }
-    else
-    {
+    }else{
         showMatrix(mat_B);
     }
 }
@@ -57,8 +53,8 @@ void showMatrix(int matrix[8][8]){
             digitalWrite(ANODEPIN[ano], matrix[ano][cat]);
         }
         // 三項演算子を他の式でもできる
-        digitalWrite(ANODEPIN[cat], LOW);
-        delay(6);
-        digitalWrite(ANODEPIN[cat], HIGH);
+        digitalWrite(CATHODEPIN[cat], LOW);
+        delay(1);
+        digitalWrite(CATHODEPIN[cat], HIGH);
     }
 }
